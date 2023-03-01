14a46fb7061e26bb55e34df4a1ad41f1ea1db30f
Himanshu <himjan0@gmail.com>
2023-03-01 18:32:33 +0530

    [firstCommit]

---
+++ dice_app/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black38,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.blueGrey.shade800,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  var leftnum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          // ignore: prefer_const_constructors
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  leftnum = 5;
+                },
+                child: Image.asset('images/dice$leftnum.png'),
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  print('2nd pressed');
+                },
+                child: Image.asset('images/dice2.png'),
+              ),
+            ),
+          ),
+          // ignore: prefer_const_constructors
+        ],
+      ),
+    );
+  }
+}
